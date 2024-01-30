0fe3541e817fef09448984a00bbd67f8e53562e7
Deekshith B <130782878+bdeekshith066@users.noreply.github.com>
2024-01-30 20:56:00 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,38 @@
+// Importing Dependies
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+
+
+ //Data Collection and Analysis
+ //IMA Diabetes Dataset
+ diabetes_dataset = pd.read_csv('/delete ml csv.csv')  
+
+
+// printing the first 5 rows of the dataset
+diabetes_dataset.head()
+
+//number of rows and columns in this dataset
+diabetes_dataset.shape
+
+
+diabetes_dataset['Outcome'].value_counts()
+
+
+//printing the first 5 rows of the dataset
+diabetes_dataset.head()
+
+
+//number of rows and columns in this dataset
+diabetes_dataset.shape
+
+
+//number of rows and columns in this dataset
+diabetes_dataset.shape
+
+
+                 
